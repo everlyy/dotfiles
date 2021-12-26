17243b4a7ac8df3137cc7868893d46ccac6a997a
Florian <66821749+flornian@users.noreply.github.com>
2021-12-26 17:07:28 +0100

    wallpaper

--- init.sh
+++ init.sh
@@ -4,7 +4,7 @@ FILES=/home/florian/Documents/dotfiles
 slstatus &
 
 # Set wallpaper.
-feh --no-fehbg --bg-fill $FILES/wallpapers/wallpaper.jpeg &
+feh --no-fehbg --bg-fill $FILES/wallpapers/Shopping.webp &
 
 # Run script to set drawing tablet area
 python3 $FILES/other/tablet.py & 
